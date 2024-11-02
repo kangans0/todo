024ecc69194827b042edd2061d068bbfa9235151
kangans0 <skangan78@gmail.com>
2024-11-03 00:50:01 +0530

    Main

--- script.js
+++ script.js
@@ -42,8 +42,58 @@ function deleteAllTasks(){
 
 function displayTasks(){
     //some logic
-    // todoList.innerHTML=
+    todoList.innerHTML="";
+    todo.forEach((item,index) => {
+        const p=document.createElement("p");
+        p.innerHTML= `
+            <div class="todo-container">
+                <input type="checkbox" class="todo-checkbox" id="input-${index}"${item.disabled ? "checked" : "" }>
+                <p id="todo-${index}" class="${item.disabled ? "disabled":""}"
+                onclick="editTask(${index})">${item.text}                
+                </p>
+            </div>
+        `;
+        p.querySelector(".todo-checkbox").addEventListener("change",()=>{
+            toggleTask(index);
+        });
+        todoList.appendChild(p);
+    });
+    todoCount.textContent= todo.length;
 }
+
+function editTask(index){
+    const todoItem=document.getElementById(`todo-${index}`);
+    const existingText = todo[index].text;
+    const inputElement=document.createElement("input");
+
+    inputElement.value=existingText;
+    todoItem.replaceWith(inputElement);
+    inputElement.focus();
+
+    inputElement.addEventListener("blur",function(){
+        const updatedText= inputElement.value.trim();
+        if(updatedText){
+            todo[index].text=updatedText;
+            saveToLocalStorage();
+        }
+        displayTasks();
+    });
+}
+
+function toggleTask(index){
+    todo[index].disabled=!todo[index].disabled;
+    saveToLocalStorage();
+    displayTasks();
+}
+
+
+function deleteAllTasks(){
+    todo=[];
+    saveToLocalStorage();
+    displayTasks();
+}
+
+
 
 function saveToLocalStorage(){
     localStorage.setItem("todo", JSON.stringify(todo));
